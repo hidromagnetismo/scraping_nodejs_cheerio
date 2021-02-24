bad50645c0e65e2e5461c41e9d4da937843da970
Liscano Natanael <hidromagnetismo@gmail.com>
2021-02-24 08:25:33 +0000

    Save data in file .csv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 const cheerio = require('cheerio');
 const request = require('request-promise');
+const fs = require('fs-extra');
+const writeStream = fs.createWriteStream('quotes.csv');
 
 async function init() {
     const $ = await request({
@@ -8,12 +10,14 @@ async function init() {
         transform: body => cheerio.load(body)
     });
     
+    writeStream.write('Quote|Author|Tags\n');
     $('.quote').each((i, el) => {
         const text = $(el).find('span.text').text().replace(/(^\“|\”$)/g, "");
         const author = $(el).find('span small.author').text();
         const tags = [];
         $(el).find('.tags a.tag').each((i, el) => tags.push($(el).text()));
-        console.log(tags.join(','));
+        // console.log(tags.join(','));
+        writeStream.write(`${text}|${author}|${tags}\n`);
     });
 }
 
